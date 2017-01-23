c34c9b0d1399e8e801b9b40c0dba5ca28976e21b
Balachandar Sankar <balas@yahoo-inc.com>
2017-01-23 10:35:05 -0800

    Initial Commit

---
+++ Flicks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flicks
+//
+//  Created by Balachandar Sankar on 1/23/17.
+//  Copyright © 2017 Balachandar Sankar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Flicks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flicks
+//
+//  Created by Balachandar Sankar on 1/23/17.
+//  Copyright © 2017 Balachandar Sankar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
